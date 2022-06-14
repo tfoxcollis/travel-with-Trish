085162a70b1e9dded47f654de8d82601d65cfafe
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-06-13 21:06:38 -0700

    toggle error message for incorrect username

--- src/signin.js
+++ src/signin.js
@@ -1,2 +1,27 @@
 import './css/signin.css';
 
+
+let signinButton = document.querySelector("#signinButton");
+let formUserName = document.querySelector("#formUserName");
+let formPassword = document.querySelector("#formPassword");
+
+const validateUserName = (event) => {
+  if(!formUserName.value.includes("traveler")){
+    event.preventDefault();
+    toggleErrorMessage();
+  }
+}
+
+// const validatePassword = () => {
+
+// }
+
+const toggleErrorMessage = (event) => {
+  let errorMessage = document.querySelector(".form-message-error");
+  errorMessage.classList.remove("hidden");
+}
+
+signinButton.addEventListener("click", (event) => {
+  validateUserName(event);
+  validatePassword();
+})
