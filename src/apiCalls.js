1a9ed3165e48bf310d1ba5c67e74d2d660e7c692
Trish Fox-Collis <tfoxcollis@gmail.com>
2023-01-10 00:31:42 -0800

    Vercel Deploy

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,9 +1,8 @@
 //apiCalls
 const getData = (apiName) => {
   return fetch(`https://travel-tracker-api-one.vercel.app/api/v1/${apiName}`, {
-    mode: 'cors',
     headers: {
-      'Access-Control-Allow-Origin':'*'
+      'Origin':'https://travel-tracker-api-one.vercel.app'
     }
   }).then(
     (response) => response.json()
@@ -14,12 +13,10 @@ const postData = (apiName, formData) => {
   let url = `https://travel-tracker-api-one.vercel.app/api/v1/${apiName}`
   return fetch(url, {
     method: "POST",
-    mode: 'cors',
-
     body: JSON.stringify(formData),
     headers: {
       'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin':'*'
+      'Origin': 'https://travel-tracker-api-one.vercel.app'
     }
   }).then(
     (response) => response.json()
